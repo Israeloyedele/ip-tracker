c92548456b9d40a9208ab800a38aea641b66c6a7
Israel Oyedele <israeloyedele01@gmail.com>
2026-03-05 07:42:19 -0500

    first commit

--- api/get-ip-details.ts
+++ api/get-ip-details.ts
@@ -13,7 +13,7 @@ export default async function getIpDetails(req: Request, res: Response): Promise
         const url: string = `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${ip}`;
         const response = await axios.get(url);
         console.log(response.data);
-        return Response.json({status: "success"});
+        return response.data;
     }
     catch(err){
         console.log(err);
