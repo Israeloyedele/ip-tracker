84201c51b853bca84b729cc22c883569488c5111
Israel Oyedele <israeloyedele01@gmail.com>
2026-03-05 11:03:38 -0500

    first commit

--- api/get-ip.ts
+++ api/get-ip.ts
@@ -11,7 +11,7 @@ export async function GET(request: Request) {
     const url: string = `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${ip}`;
     const response = await axios.get(url)
         .then((response: AxiosResponse) => {
-        console.log(response);
+        console.log(response.data);
     }).catch((error: AxiosError) => {
         console.log(error.message);
         });
--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
     useEffect(()=> {
         axios.get("/api/get-ip")
-            .then(res=> console.log(res.data.ip))
+            .then(res=> console.log(res.data))
             .catch(err => console.log(err));
     },[])
 
