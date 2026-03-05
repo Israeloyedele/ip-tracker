24f08db83735f73e8e68f2299664f15b0f55960e
Israel Oyedele <israeloyedele01@gmail.com>
2026-03-05 03:59:44 -0500

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-// @ts-ignore
-  plugins: [  tailwindcss(), react() ]
-
+  plugins: [
+      tailwindcss(),
+      react()
+  ]
 })
