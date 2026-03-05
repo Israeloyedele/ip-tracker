fa3876977c59bc56154c95799a4340a635ae65d2
Israel Oyedele <israeloyedele01@gmail.com>
2026-03-05 08:01:04 -0500

    first commit

--- api/get-ip-details.ts
+++ api/get-ip-details.ts
@@ -3,20 +3,18 @@ import axios from "axios";
 const API_KEY: string = process.env.API_KEY;
 type Body = { ip: string; }
 
-export default async function getIpDetails(req: Request, res: Response): Promise<Response> {
+export async function GET(req: Request): Promise<Response> {
     if(req.method !== "POST") {
         return new Response(JSON.stringify({ error: "Method Not Allowed" }));
     }
-
     const { ip } = req.body as unknown as Body;
     try {
         const url: string = `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${ip}`;
         const response = await axios.get(url);
         console.log(response.data);
-        return response.data;
+        return Response.json(response.data);
     }
     catch(err){
         console.log(err);
-        return Response.json({status: "error", error: err});
     }
 }
