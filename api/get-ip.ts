4872224e1459e28d847e9d3e8ccf2f5570242f37
Israel Oyedele <israeloyedele01@gmail.com>
2026-03-05 11:00:04 -0500

    first commit

--- api/get-ip.ts
+++ api/get-ip.ts
@@ -1,4 +1,4 @@
-import axios, {AxiosResponse} from "axios";
+import axios, {AxiosError, AxiosResponse} from "axios";
 
 const API_KEY: string = process.env.API_KEY;
 
@@ -7,18 +7,15 @@ export async function GET(request: Request) {
     const ip =
         request.headers.get("x-forwarded-for")?.split(",")[0] ??
         "Unknown";
-    let response:AxiosResponse;
     console.log(request.headers)
+    const url: string = `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${ip}`;
+    const response = await axios.get(url)
+        .then((response: AxiosResponse) => {
+        console.log(response);
+    }).catch((error: AxiosError) => {
+        console.log(error.message);
+        });
 
-
-    try {
-        const url: string = `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${ip}`;
-        response  = await axios.get(url);
-
-    }
-    catch(err){
-        console.log(err);
-    }
 
     return Response.json({ ip, response });
 }
