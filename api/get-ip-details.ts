8efe2b85a4f29d8779a53b16b8456159b3c12b3c
Israel Oyedele <israeloyedele01@gmail.com>
2026-03-05 10:52:05 -0500

    first commit

--- api/get-ip-details.ts
+++ api/get-ip-details.ts
@@ -1,24 +1,22 @@
-import axios from "axios";
+// import axios from "axios";
+//
+//
+//
+// export async function POST(req: Request): Promise<Response> {
+//     if(req.method !== "POST") {
+//         console.log(req);
+//         return new Response(JSON.stringify({ error: "Method Not Allowed" }));
+//     }
+//
+//     try {
+//         console.log(ip);
+//     }
+//     catch(err){
+//         console.log(err);
+//     }
+// }
 
-const API_KEY: string = process.env.API_KEY;
-type Body = { ip: string; }
 
-export async function POST(req: Request): Promise<Response> {
-    if(req.method !== "POST") {
-        console.log(req);
-        return new Response(JSON.stringify({ error: "Method Not Allowed" }));
-    }
-    const { ip } = req.body as unknown as Body;
-    try {
-        console.log(ip);
-    }
-    catch(err){
-        console.log(err);
-    }
-}
-
-
-// const url: string = `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${ip}`;
-// const response = await axios.get(url);
+//
 // console.log(response);
 // return Response.json(response);
--- api/get-ip.ts
+++ api/get-ip.ts
@@ -1,6 +1,24 @@
-export function GET(request: Request) : Response {
+import axios, {AxiosResponse} from "axios";
+
+const API_KEY: string = process.env.API_KEY;
+
+
+export async function GET(request: Request) {
     const ip =
         request.headers.get("x-forwarded-for")?.split(",")[0] ??
         "Unknown";
-    return Response.json({ ip });
+    let response:AxiosResponse;
+    console.log(request.headers)
+
+
+    try {
+        const url: string = `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${ip}`;
+        response  = await axios.get(url);
+
+    }
+    catch(err){
+        console.log(err);
+    }
+
+    return Response.json({ ip, response });
 }
